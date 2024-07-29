0c74cf0aab82613360ff424c48142f83b6c64490
felipe <083felype@gmail.com>
2024-07-28 23:49:31 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: '#FAFAFA',
+        yellow: '#DBAC2C',
+        purple: '#8047F8',
+        white: '#FFFFFF',
+        'yellow-dark': '#C47F17',
+        'yellow-light': '#F1E9C9',
+        'purple-dark': '#4B2995',
+        'purple-light': '#EBE5F9',
+        'base-title': '#272221',
+        'base-subtitle': '#403937',
+        'base-text': '#574F4D',
+        'base-label': '#8D8686',
+        'base-hover': '#D7D5D5',
+        'base-button': '#E6E5E5',
+        'base-input': '#EDEDED',
+        'base-card': '#F3F2F2',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
